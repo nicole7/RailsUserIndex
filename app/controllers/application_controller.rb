68dc23ccb5460c1b7a7b88f4c8b076437fd50472
Nicole-Gasperini <nicole.gasperini88@gmail.com>
2018-08-18 13:11:04 -0600

    Wrong modifications on devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  protected
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
   end
-
 end
