3a885a16937d5320f2507eedabc92f99647b35bf
Nicole-Gasperini <nicole.gasperini88@gmail.com>
2018-08-19 14:26:56 -0600

    Working on error setup for controller testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation,
-    #     :current_password])
    additional_params = [:first_name, :last_name]
   devise_parameter_sanitizer.permit(:sign_up, keys: additional_params)
   devise_parameter_sanitizer.permit(:account_update, keys: additional_params)
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name "Nicole"
+    last_name "Gasperini"
+    fav_color "Yellow"
+    email "gasperini@gmail.com"
+    password "123456"
+  end
+end
